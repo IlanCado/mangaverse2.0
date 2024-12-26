69f7184491dd82dd6cb0f472c8785f56c10d1bca
Ilan <ilancado99@gmail.com>
2024-12-26 14:37:32 +0100

    Ajout favicon et logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Mangaverse')</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="{{ asset('images/logo.png') }}" type="image/png">
+
+    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- FontAwesome pour les étoiles interactives -->
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+
     <style>
         body {
             background-image: url('/images/back.png');
@@ -70,46 +76,52 @@
 <body class="d-flex flex-column min-vh-100">
     <!-- Navbar -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
-        <div class="container">
-            <a class="navbar-brand fw-bold" href="{{ route('home') }}">Mangaverse</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+    <div class="container">
+        <!-- Logo + Brand -->
+        <a class="navbar-brand d-flex align-items-center" href="{{ route('home') }}">
+            <img src="{{ asset('images/logo.png') }}" alt="Logo" style="width: 30px; height: 30px; margin-right: 10px;">
+            <span>Mangaverse</span>
+        </a>
+        
+        <!-- Navbar Toggler -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
 
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto align-items-center">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto align-items-center">
+                <li class="nav-item mx-2">
+                    <a class="nav-link text-white" href="{{ route('home') }}">Explorer les Mangas</a>
+                </li>
+                @auth
                     <li class="nav-item mx-2">
-                        <a class="nav-link text-white" href="{{ route('home') }}">Explorer les Mangas</a>
+                        <a class="nav-link text-white" href="{{ route('dashboard') }}">Profil</a>
                     </li>
-                    @auth
+                    <li class="nav-item mx-2">
+                        <a class="btn btn-success text-white px-3" href="{{ route('mangas.create') }}">Ajouter un Manga</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <form method="POST" action="{{ route('logout') }}" class="d-inline">
+                            @csrf
+                            <button class="btn btn-danger text-white px-3" type="submit">Se déconnecter</button>
+                        </form>
+                    </li>
+                    @if(auth()->user()->is_admin)
                         <li class="nav-item mx-2">
-                            <a class="nav-link text-white" href="{{ route('dashboard') }}">Profil</a>
+                            <a class="nav-link text-warning fw-bold" href="{{ route('admin.index') }}">Administration</a>
                         </li>
-                        <li class="nav-item mx-2">
-                            <a class="btn btn-success text-white px-3" href="{{ route('mangas.create') }}">Ajouter un Manga</a>
-                        </li>
-                        <li class="nav-item mx-2">
-                            <form method="POST" action="{{ route('logout') }}" class="d-inline">
-                                @csrf
-                                <button class="btn btn-danger text-white px-3" type="submit">Se déconnecter</button>
-                            </form>
-                        </li>
-                        @if(auth()->user()->is_admin)
-                            <li class="nav-item mx-2">
-                                <a class="nav-link text-warning fw-bold" href="{{ route('admin.index') }}">Administration</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item mx-2">
-                            <a class="nav-link text-white" href="{{ route('login') }}">Se connecter</a>
-                        </li>
-                        <li class="nav-item mx-2">
-                            <a class="nav-link text-white" href="{{ route('register') }}">S'inscrire</a>
-                        </li>
-                    @endauth
-                </ul>
-            </div>
+                    @endif
+                @else
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-white" href="{{ route('login') }}">Se connecter</a>
+                    </li>
+                    <li class="nav-item mx-2">
+                        <a class="nav-link text-white" href="{{ route('register') }}">S'inscrire</a>
+                    </li>
+                @endauth
+            </ul>
         </div>
+    </div>
     </nav>
 
     <!-- Contenu principal -->
